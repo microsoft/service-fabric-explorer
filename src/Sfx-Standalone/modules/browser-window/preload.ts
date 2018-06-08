5b7bc62fb6f1a34c0f94dcf8fd34454c7e2f036b
Steven Shan <stevshan@microsoft.com>
2018-06-07 17:09:58 -0700

    browser-window preload.

--- src/Sfx-Standalone/modules/browser-window/preload.ts
+++ src/Sfx-Standalone/modules/browser-window/preload.ts
@@ -3,7 +3,4 @@
 // Licensed under the MIT License. See License file under the project root for license information.
 //-----------------------------------------------------------------------------
 
-import { ModuleManagerAgent } from "../../module-manager/module-manager-host";
-
-global["exports"] = exports;
-global["moduleManager"] = new ModuleManagerAgent();
+import "../../../module-manager/bootstrap";
