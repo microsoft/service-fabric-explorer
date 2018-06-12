9438a317074e900eae7451aa9e643d17596300ac
Steven Shan <stevshan@microsoft.com>
2018-06-12 16:22:01 -0700

    Fix the license title & the menu visibility issue.

--- src/Sfx-Standalone/module-manager/module-manager.d.ts
+++ src/Sfx-Standalone/module-manager/module-manager.d.ts
@@ -1,3 +1,7 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License. See License file under the project root for license information.
+//-----------------------------------------------------------------------------
 
 declare module "sfx.module-manager" {
     import { ICommunicator } from "sfx.remoting";
