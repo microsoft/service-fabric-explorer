a20da014ca0af03c346fe919e3af709d9457525d
Steven Shan <stevshan@microsoft.com>
2018-09-20 01:05:24 -0700

    save.

--- src/Sfx-Standalone/gulpfile.js
+++ src/Sfx-Standalone/gulpfile.js
@@ -4,5 +4,16 @@
 //-----------------------------------------------------------------------------
 
 const gulp = require("gulp");
+const { execSync } = require("child_process");
+
+const configs = require("./.build/configs");
+
+gulp.task("build:node_modules",
+    () => {
+        console.log("NPM", "Executing", `${configs.buildInfos.paths.buildDir}> npm install --production`);
+        console.log(execSync("npm install --production", { cwd: configs.buildInfos.paths.buildDir, encoding: "utf8" }));
+
+        return Promise.resolve();
+    });
 
 require("./.build");
