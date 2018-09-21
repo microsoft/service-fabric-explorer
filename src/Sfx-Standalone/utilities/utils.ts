dfd1eb46c7a83b3bad818144f1521ecf6289d653
Steven Shan <stevshan@microsoft.com>
2018-09-20 19:55:43 -0700

    Fix isSerializable() logics on checking if a value is serializable.

--- src/Sfx-Standalone/utilities/utils.ts
+++ src/Sfx-Standalone/utilities/utils.ts
@@ -78,7 +78,10 @@ Object.isSerializable = (value: any) => {
                 return value.every((itemValue) => Object.isSerializable(itemValue));
             }
 
-            return Object.values(value).every((propertyValue) => Object.isSerializable(propertyValue));
+            const valuePrototype = Object.getPrototypeOf(value);
+
+            return (!valuePrototype || valuePrototype === Object.prototype)
+                && Object.values(value).every((propertyValue) => Object.isSerializable(propertyValue));
 
         case "undefined":
         case "number":
