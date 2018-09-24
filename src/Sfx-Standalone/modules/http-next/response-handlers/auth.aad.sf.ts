697dcb1d383f4b08f9cbeffcb23884e5a224be48
Steven Shan <stevshan@microsoft.com>
2018-09-23 19:16:20 -0700

    save.

---
+++ src/Sfx-Standalone/modules/http-next/response-handlers/auth.aad.sf.ts
@@ -0,0 +1,60 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License. See License file under the project root for license information.
+//-----------------------------------------------------------------------------
+import {
+    IHttpPipeline,
+    IHttpRequest,
+    IHttpResponse,
+    HttpResponseHandler
+} from "sfx.http-next";
+
+import { handleResponseAsync as handleAadAsync } from "./auth.aad";
+
+import * as url from "url";
+
+interface ISfAadMetadata {
+    type: string;
+    authority: string;
+    client: string;
+    cluster: string;
+    login: string;
+    redirect: string;
+    tenant: string;
+}
+
+async function handleResponseAsync(pipeline: IHttpPipeline, request: IHttpRequest, response: IHttpResponse): Promise<IHttpResponse> {
+    if (response.statusCode !== 401 && response.statusCode !== 403) {
+        return undefined;
+    }
+
+    const aadMetadataResponse = await pipeline.requestAsync(
+        {
+            method: "GET",
+            url: url.resolve(request.url, "/$/GetAadMetadata?api-version=1.0")
+        });
+
+    if (aadMetadataResponse.statusCode !== 200) {
+        return undefined;
+    }
+
+    const aadMetadata: ISfAadMetadata = aadMetadataResponse.result;
+
+    if (aadMetadata.type !== "aad") {
+        return undefined;
+    }
+
+    await handleAadAsync(
+        {
+            authority: aadMetadata.authority,
+            redirectUri: url.resolve(request.url, "/Explorer/index.html"),
+            clientId: aadMetadata.cluster
+        }, 
+        pipeline, 
+        request, 
+        response);
+}
+
+export default function createResponseHandler(): HttpResponseHandler {
+    return handleResponseAsync;
+}
