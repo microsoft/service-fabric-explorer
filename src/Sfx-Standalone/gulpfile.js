ed7d2cad1e8a95af274d57e37f05561f5f798cfe
Steven Shan <stevshan@microsoft.com>
2018-09-26 14:56:04 -0700

    add smart npm install.

--- src/Sfx-Standalone/gulpfile.js
+++ src/Sfx-Standalone/gulpfile.js
@@ -4,16 +4,30 @@
 //-----------------------------------------------------------------------------
 
 const gulp = require("gulp");
-const { execSync } = require("child_process");
 
-const configs = require("cookie.gulp/configs");
+gulp.task("build@node-modules",
+    () => new Promise((resolve, reject) => {
+        const { execSync } = require("child_process");
+        const configs = require("cookie.gulp/configs");
+        const fs = require("fs");
+        const path = require("path");
 
-gulp.task("pack@node-modules",
-    () => {
+        try {
+            const stat = fs.statSync(path.join(configs.buildInfos.paths.buildDir, "node_modules"));
+
+            if (stat.isDirectory()) {
+                return resolve();
+            }
+        } catch (error) {
+            if (!error || error.code !== "ENOENT") {
+                return reject(error);
+            }
+        }
+
         console.log("NPM", "Executing", `${configs.buildInfos.paths.buildDir}> npm install --production`);
         console.log(execSync("npm install --production", { cwd: configs.buildInfos.paths.buildDir, encoding: "utf8" }));
 
-        return Promise.resolve();
-    });
+        resolve();
+    }));
 
 require("cookie.gulp")();
