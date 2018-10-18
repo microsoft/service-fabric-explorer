bc31411887ab1f2e6945b9239d64f73038fd3f55
Steven Shan <34558962+stevshan@users.noreply.github.com>
2018-10-17 17:37:35 -0700

    Add two build targets for codesign for windows. (#133)
    * move build@node-modules to cookie.gulp.
    * Add new npm task for updating the package version.

--- src/Sfx-Standalone/gulpfile.js
+++ src/Sfx-Standalone/gulpfile.js
@@ -5,29 +5,4 @@
 
 const gulp = require("gulp");
 
-gulp.task("build@node-modules",
-    () => new Promise((resolve, reject) => {
-        const { execSync } = require("child_process");
-        const configs = require("cookie.gulp/configs");
-        const fs = require("fs");
-        const path = require("path");
-
-        try {
-            const stat = fs.statSync(path.join(configs.buildInfos.paths.buildDir, "node_modules"));
-
-            if (stat.isDirectory()) {
-                return resolve();
-            }
-        } catch (error) {
-            if (!error || error.code !== "ENOENT") {
-                return reject(error);
-            }
-        }
-
-        console.log("NPM", "Executing", `${configs.buildInfos.paths.buildDir}> npm install --production`);
-        console.log(execSync("npm install --production", { cwd: configs.buildInfos.paths.buildDir, encoding: "utf8" }));
-
-        resolve();
-    }));
-
 require("cookie.gulp")(gulp.registry());
