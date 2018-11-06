a8477d71bfebd93987976e02af39b948927c21d3
ammarZaidi <zaidi1039@gmail.com>
2018-11-05 16:03:31 -0800

    Adding networks information to SFX (#142)
    * adding networks information

--- src/Sfx/App/Scripts/Common/ResponseMessageHandlers.ts
+++ src/Sfx/App/Scripts/Common/ResponseMessageHandlers.ts
@@ -44,6 +44,26 @@ module Sfx {
         }
     }
 
+    export class PutResponseMessageHandler extends GetResponseMessageHandler {
+        public getSuccessMessage(apiDesc: string, response: ng.IHttpPromiseCallbackArg<any>): string {
+            return `${apiDesc}`;
+        }
+
+        public getErrorMessage(apiDesc: string, response: ng.IHttpPromiseCallbackArg<any>): string {
+            return this.getErrorMessageInternal(apiDesc, response);
+        }
+    }
+
+    export class DeleteResponseMessageHandler extends GetResponseMessageHandler {
+        public getSuccessMessage(apiDesc: string, response: ng.IHttpPromiseCallbackArg<any>): string {
+            return `${apiDesc}`;
+        }
+
+        public getErrorMessage(apiDesc: string, response: ng.IHttpPromiseCallbackArg<any>): string {
+            return this.getErrorMessageInternal(apiDesc, response);
+        }
+    }
+
     export class SilentResponseMessageHandler implements IResponseMessageHandler {
         public getSuccessMessage(apiDesc: string, response: ng.IHttpPromiseCallbackArg<any>): string {
             return null;
@@ -57,8 +77,9 @@ module Sfx {
     export class ResponseMessageHandlers {
         public static getResponseMessageHandler: IResponseMessageHandler = new GetResponseMessageHandler();
         public static postResponseMessageHandler: IResponseMessageHandler = new PostResponseMessageHandler();
-        public static putResponseMessageHandler: IResponseMessageHandler = new PostResponseMessageHandler();
+        public static putResponseMessageHandler: IResponseMessageHandler = new PutResponseMessageHandler();
         public static silentResponseMessageHandler: IResponseMessageHandler = new SilentResponseMessageHandler();
+        public static deleteResponseMessageHandler: IResponseMessageHandler = new DeleteResponseMessageHandler();
     }
 
     export class EventsStoreResponseMessageHandler implements IResponseMessageHandler {
