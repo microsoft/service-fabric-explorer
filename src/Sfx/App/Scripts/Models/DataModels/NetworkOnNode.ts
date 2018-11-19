e34df3e89cb8057addfce37f325eec8999c4dd1d
tomchen <codetom@outlook.com>
2018-11-19 15:24:08 -0800

    avoid loading image store content again while it is loading; UI refactoring

--- src/Sfx/App/Scripts/Models/DataModels/NetworkOnNode.ts
+++ src/Sfx/App/Scripts/Models/DataModels/NetworkOnNode.ts
@@ -1,3 +1,7 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
 module Sfx {
 
     export class NetworkOnNode extends DataModelBase<IRawNetworkOnNode> {
