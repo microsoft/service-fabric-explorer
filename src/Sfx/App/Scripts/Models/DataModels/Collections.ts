e228fb7f8ed6da360d9962d260c273177d339f6a
jeffj6123 <jeffrey.jarry@gmail.com>
2019-07-31 14:38:08 -0700

    one node / degraded seed node issue.

--- src/Sfx/App/Scripts/Models/DataModels/Collections.ts
+++ src/Sfx/App/Scripts/Models/DataModels/Collections.ts
@@ -317,8 +317,18 @@ module Sfx {
 
             this.disabledNodes = `${disabledNodes}/${disablingNodes}`;
             this.seedNodeCount = seedNodes.length;
+
+            this.checkSeedNodeCount(this.seedNodeCount);
+            this.checkOneNodeScenario();
+
+            this.healthySeedNodes = seedNodes.length.toString() + " (" +
+                Math.round(healthyNodes.length / seedNodes.length * 100).toString() + "%)";
+                
+        }
+
+        private checkSeedNodeCount(count: number){
             //if < 5 seed nodes display warning
-            if (this.seedNodeCount < 5) {
+            if (count < 5) {
                 this.data.warnings.addOrUpdateNotification({
                     message: "Cluster is degraded because it does not have 5 seed nodes. See link for more details",
                     level: StatusWarningLevel.Error,
@@ -329,9 +339,20 @@ module Sfx {
             }else {
                 this.data.warnings.removeNotificationById("degradedClusterState");
             }
+        }
 
-            this.healthySeedNodes = seedNodes.length.toString() + " (" +
-                Math.round(healthyNodes.length / seedNodes.length * 100).toString() + "%)";
+        private checkOneNodeScenario(): void{
+            if(this.collection.length === 1){
+                this.data.warnings.addOrUpdateNotification({
+                    message: "One node cluster is considered test and can not perform cluster upgrades.",
+                    level: StatusWarningLevel.Info,
+                    priority: 1,
+                    id: "oneNodeCluster",
+                    link: "https://aka.ms/servicefabric/durability"
+                });
+            }else{
+                this.data.warnings.removeNotificationById("oneNodeCluster");
+            }
         }
 
         private updateNodesHealthState(): void {
