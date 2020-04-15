78bfd9b5e99f9c47ac0ff988c124324df3c12e56
jeffj6123 <jeffrey.jarry@gmail.com>
2020-04-15 12:54:56 -0700

    can not properly create app.

---
+++ src/SfxWeb/src/app/views/application-type/create-application/create-application.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IsolatedAction } from 'src/app/Models/Action';
+import { ApplicationType } from 'src/app/Models/DataModels/ApplicationType';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Constants } from 'src/app/Common/Constants';
+
+@Component({
+  selector: 'app-create-application',
+  templateUrl: './create-application.component.html',
+  styleUrls: ['./create-application.component.scss']
+})
+export class CreateApplicationComponent implements OnInit {
+
+  app: ApplicationType
+  form: FormGroup;
+
+  constructor(public dialogRef: MatDialogRef<CreateApplicationComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IsolatedAction,
+    private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+   this.app = this.data.data.appType;
+   this.form = this.formBuilder.group({
+    userInput: [Constants.FabricPrefix + this.app.name, [Validators.required, Validators.pattern(/^fabric:\/.+/)]]
+    })
+
+    this.form.invalid
+  }
+
+  ok(){
+    this.app.createInstance(this.form.value.userInput).subscribe(
+      () => this.close(),
+      () => console.log("failed to create application")
+    )
+  }
+
+  close() {
+    this.dialogRef.close(false);    
+  }
+
+}
