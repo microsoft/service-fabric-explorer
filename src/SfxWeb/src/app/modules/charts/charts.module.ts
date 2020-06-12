ef9d5d82c5733e421ea6c4837582e8970fbb26de
jeffj6123 <jeffrey.jarry@gmail.com>
2020-06-12 16:30:06 -0700

    adding links to dashboard tiles.

--- src/SfxWeb/src/app/modules/charts/charts.module.ts
+++ src/SfxWeb/src/app/modules/charts/charts.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardTileComponent } from './dashboard-tile/dashboard-tile.component';
 import { DashboardTextTileComponent } from './dashboard-text-tile/dashboard-text-tile.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [DashboardTileComponent, DashboardTextTileComponent, BarChartComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [DashboardTileComponent, DashboardTextTileComponent, BarChartComponent]
 })
