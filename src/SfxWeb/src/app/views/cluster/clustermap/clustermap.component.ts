3512d163e55f0440a69e6a0f486939d31c6a2211
jeffj6123 <jeffrey.jarry@gmail.com>
2020-07-20 17:02:46 -0700

    use constants

--- src/SfxWeb/src/app/views/cluster/clustermap/clustermap.component.ts
+++ src/SfxWeb/src/app/views/cluster/clustermap/clustermap.component.ts
@@ -1,10 +1,11 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { IResponseMessageHandler } from 'src/app/Common/ResponseMessageHandlers';
 import { forkJoin, Observable } from 'rxjs';
 import { Node } from 'src/app/Models/DataModels/Node';
 import { BaseController } from 'src/app/ViewModels/BaseController';
 import { NodeCollection } from 'src/app/Models/DataModels/collections/NodeCollection';
+import { HealthStateConstants } from 'src/app/Common/Constants';
 
 @Component({
   selector: 'app-clustermap',
@@ -15,25 +16,25 @@ export class ClustermapComponent extends BaseController {
 
   nodes: NodeCollection;
   filter: string = "";
-  healthFilter = {
-    Ok: true,
-    Warning: true,
-    Error: true
-  }
+  healthFilter: Record<string, boolean> = {}
 
   constructor(private data: DataService, injector: Injector) {
+
     super(injector);
    }
 
    setup(){
     this.nodes = this.data.nodes;
+    this.healthFilter[HealthStateConstants.OK] = true;
+    this.healthFilter[HealthStateConstants.Warning] = true;
+    this.healthFilter[HealthStateConstants.Error] = true;
    }
 
-    refresh(messageHandler?: IResponseMessageHandler): Observable<any> {
-      return forkJoin([
-        this.nodes.refresh(messageHandler)
-      ])
-    }
+  refresh(messageHandler?: IResponseMessageHandler): Observable<any> {
+    return forkJoin([
+      this.nodes.refresh(messageHandler)
+    ])
+  }
 
   public getNodesForDomains(upgradeDomain: string, faultDomain: string): Node[] {
     return this.nodes.collection.filter((node) => node.upgradeDomain === upgradeDomain && 
