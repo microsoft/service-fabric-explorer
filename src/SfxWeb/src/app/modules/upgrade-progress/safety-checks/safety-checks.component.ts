8ab03d9f09809aea7833251e546e289265e72747
jeffj6123 <jeffrey.jarry@gmail.com>
2020-09-02 10:34:17 -0700

    clean up.

--- src/SfxWeb/src/app/modules/upgrade-progress/safety-checks/safety-checks.component.ts
+++ src/SfxWeb/src/app/modules/upgrade-progress/safety-checks/safety-checks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { IRawSafetyCheckDescription } from 'src/app/Models/RawDataTypes';
 import { RestClientService } from 'src/app/services/rest-client.service';
 import { IPartitionData } from '../partition-info/partition-info.component';
@@ -10,26 +10,22 @@ import { MessageService, MessageSeverity } from 'src/app/services/message.servic
   styleUrls: ['./safety-checks.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SafetyChecksComponent implements OnInit {
+export class SafetyChecksComponent {
 
   @Input() safetyChecks: IRawSafetyCheckDescription[];
   //This can be provided to share a cache between safety check components
   @Input() partitions: Record<string, IPartitionData> = {}
 
   constructor(private restClientService: RestClientService,
-    private cdr: ChangeDetectorRef,
-    private messageService: MessageService) { }
+              private cdr: ChangeDetectorRef,
+              private messageService: MessageService) { }
 
-  ngOnInit(): void {
-  }
-
-  
   async getPartitionInfo(id: string) {
     try {
       const partition = await this.restClientService.getPartitionById(id).toPromise();
       const serviceName = await this.restClientService.getServiceNameInfo(id).toPromise();
       const applicationName = await this.restClientService.getApplicationNameInfo(serviceName.Id).toPromise();
-  
+
       this.partitions[id] = {
         serviceName,
         applicationName,
