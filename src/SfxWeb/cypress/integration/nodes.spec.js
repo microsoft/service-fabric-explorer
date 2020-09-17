7a6a281aea109c3b0ae1b782dd7c62f6c2a17722
jeffj6123 <jeffrey.jarry@gmail.com>
2020-09-17 11:52:31 -0700

    basic nodes list page.

---
+++ src/SfxWeb/cypress/integration/nodes.spec.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+import { apiUrl, addDefaultFixtures, checkTableSize, FIXTURE_REF_APPS, refresh, FIXTURE_REF_NODES } from './util';
+
+context('nodes list page', () => {
+
+    beforeEach(() => {
+
+        cy.server()
+        addDefaultFixtures();
+        cy.visit('/#/nodes')
+
+    })
+
+    describe("essentials", () => {
+        it.only('load essentials', () => {
+            cy.wait(FIXTURE_REF_NODES);
+
+            cy.get('[data-cy=header]').within(() => {
+                cy.contains('Nodes').click();
+              });
+
+            cy.get('[data-cy=nodesList]').within(() => {
+                checkTableSize(5);
+            })
+        })
+
+    })
+
+    describe("events", () => {
+        it('view events', () => {
+            cy.wait(FIXTURE_REF_NODES);
+
+            cy.get('[data-cy=navtabs]').within(() => {
+                cy.contains('events').click();
+            })
+    
+            cy.url().should('include', 'events')
+        })
+    })
+
+})
