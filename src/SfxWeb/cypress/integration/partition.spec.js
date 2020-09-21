187ecf592fabda9867e45931485d43c8d7d73e10
jeffj6123 <jeffrey.jarry@gmail.com>
2020-09-21 13:50:56 -0700

    bare minimum partition test

---
+++ src/SfxWeb/cypress/integration/partition.spec.js
@@ -0,0 +1,66 @@
+/// <reference types="cypress" />
+
+import { addDefaultFixtures, apiUrl, FIXTURE_REF_MANIFEST } from './util';
+
+const serviceName = "VisualObjects.ActorService";
+const partitionId = "28bfaf73-37b0-467d-9d47-d011b0aedbc0";
+const appName = "VisualObjectsApplicationType";
+
+const waitRequest = "@partitionInfo";
+
+/*
+Default to stateful service for the page
+*/
+
+const routeFormatter = (appName, serviceName) => `/Applications/${appName}/$/GetServices/${appName}/${serviceName}/$/GetPartitions`;
+const urlFormatter = (app, service, partition) => `/#/apptype/${app}/app/${app}/service/${app}%252F${service}/partition/${partition}`;
+
+
+context('service', () => {
+    beforeEach(() => {
+        cy.server()
+        addDefaultFixtures();
+        cy.route(apiUrl(`/Applications/${appName}/$/GetServices?*`), "fx:app-page/services").as("services")
+
+    })
+
+    describe("stateful", () => {
+        beforeEach(() => {
+            cy.route(apiUrl(`${routeFormatter(appName, serviceName)}?*`), "fx:partition-page/partitions").as("partitions");
+            cy.route(apiUrl(`${routeFormatter(appName, serviceName)}/${partitionId}?*`), "fx:partition-page/partition-info").as("partitionInfo");
+            cy.route(apiUrl(`${routeFormatter(appName, serviceName)}/${partitionId}/$/GetReplicas?*`), "fx:partition-page/replicas").as("replicasList");
+            cy.route(apiUrl(`${routeFormatter(appName, serviceName)}/${partitionId}/$/GetHealth?*`), "fx:partition-page/health").as("health");
+
+            cy.visit(urlFormatter(appName, serviceName, partitionId))
+        })
+
+        it('load essentials', () => {
+            cy.wait(waitRequest);
+
+            cy.get('[data-cy=header]').within(() => {
+                cy.contains(partitionId).click();
+            })
+        })
+
+        it('view details', () => {
+            cy.wait(waitRequest);
+
+            cy.get('[data-cy=navtabs]').within(() => {
+                cy.contains('details').click();
+            })
+
+            cy.url().should('include', '/details')
+        })
+
+        it('view events', () => {
+            cy.wait(waitRequest);
+
+            cy.get('[data-cy=navtabs]').within(() => {
+                cy.contains('events').click();
+            })
+
+            cy.url().should('include', '/events')
+        })
+
+    })
+})
