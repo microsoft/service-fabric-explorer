b385c1b27daaf32ad1c03c074d34dd7532406c12
jeffj6123 <jeffrey.jarry@gmail.com>
2020-09-22 15:09:34 -0700

    bare tests (#401)

---
+++ src/SfxWeb/cypress/integration/deployedService.spec.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+import { addDefaultFixtures, apiUrl } from './util';
+
+const nodeName = "_nt_2"
+const appName = "VisualObjectsApplicationType";
+const serviceName = "VisualObjects.ActorServicePkg";
+const waitRequest = "@info";
+
+context('app', () => {
+    beforeEach(() => {
+        cy.server()
+        addDefaultFixtures();
+
+        cy.route(apiUrl(`Nodes/${nodeName}/$/GetApplications?*`), 'fx:deployed-service/deployed-apps').as('apps');
+        cy.route(apiUrl(`Nodes/${nodeName}/$/GetApplications/${appName}/$/GetServicePackages/${serviceName}/$/GetHealth?*`), 'fx:deployed-service/health').as('health');
+        cy.route(apiUrl(`Nodes/${nodeName}/$/GetApplications/${appName}/$/GetServicePackages/${serviceName}?*`), 'fx:deployed-service/service-info').as('info');
+        cy.route(apiUrl(`Nodes/${nodeName}/$/GetApplications/${appName}/$/GetServicePackages?*`), 'fx:deployed-service/services').as('services');
+
+        cy.route(apiUrl(`ApplicationTypes/${appName}/$/GetServiceManifest?*`), 'fx:deployed-service/manifest').as('manifest');
+
+        cy.visit(`/#/node/_nt_2/deployedapp/${appName}/deployedservice/${serviceName}`)
+    })
+
+    describe("essentials", () => {
+        it('load essentials', () => {
+            cy.wait(waitRequest);
+            cy.get('[data-cy=header').within(() => {
+                cy.contains(serviceName).click();
+            })
+
+        })
+
+    })
+
+    describe("details", () => {
+        it('view details', () => {
+            cy.wait(waitRequest);
+
+            cy.get('[data-cy=navtabs]').within(() => {
+                cy.contains('details').click();
+            })
+
+            cy.url().should('include', '/details')
+        })
+    })
+
+    describe("manifest", () => {
+        it('view manifest', () => {
+            cy.wait(waitRequest);
+
+            cy.get('[data-cy=navtabs]').within(() => {
+                cy.contains('manifest').click();
+            })
+
+            cy.wait("@manifest")
+            cy.url().should('include', '/manifest')
+        })
+    })
+})
