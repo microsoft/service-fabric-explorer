7acb92c4f3f825ae01c506ea4052d830dc22e7c4
jeffj6123 <jeffrey.jarry@gmail.com>
2020-11-04 10:18:37 -0800

    linting fix

--- src/SfxWeb/src/app/services/rest-client.service.ts
+++ src/SfxWeb/src/app/services/rest-client.service.ts
@@ -861,15 +861,15 @@ export class RestClientService {
         const header = `${err.status.toString()} : ${apiDesc}`;
 
 
-            const message = messageHandler.getErrorMessage(apiDesc, err);
-            if (message) {
+        const message = messageHandler.getErrorMessage(apiDesc, err);
+        if (message) {
                     this.message.showMessage(message, MessageSeverity.Err, header);
                 }
-    
+
             else if (err.error instanceof Error) {
                 // A client-side or network error occurred. Handle it accordingly.
             this.message.showMessage(err.error.message, MessageSeverity.Err, header);
-    
+
             } else {
                 // The backend returned an unsuccessful response code.
                 // The response body may contain clues as to what went wrong,
