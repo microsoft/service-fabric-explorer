bf2e39322ee12f3ef693d9bfd5649eeacff0db69
jeffj6123 <jeffrey.jarry@gmail.com>
2021-02-05 16:56:35 -0800

    linted

--- src/SfxWeb/src/app/error-handling.ts
+++ src/SfxWeb/src/app/error-handling.ts
@@ -1,12 +1,12 @@
-import { Injectable, ErrorHandler } from "@angular/core";
-import { TelemetryService } from "./services/telemetry.service";
+import { Injectable, ErrorHandler } from '@angular/core';
+import { TelemetryService } from './services/telemetry.service';
 
 @Injectable()
 export class AppInsightsErrorHandler implements ErrorHandler {
 constructor(private telemetry: TelemetryService) {}
 
-    handleError(error) {       
+    handleError(error) {
         this.telemetry.appInsights.trackException(error);
-        console.error(error)
+        console.error(error);
     }
 }
