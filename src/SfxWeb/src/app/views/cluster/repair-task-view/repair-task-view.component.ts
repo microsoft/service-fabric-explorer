d77a2ecc03abb47e5749da24988017a3895ef7eb
jeffj6123 <jeffrey.jarry@gmail.com>
2021-03-03 15:09:49 -0800

    added proper css classes.

--- src/SfxWeb/src/app/views/cluster/repair-task-view/repair-task-view.component.ts
+++ src/SfxWeb/src/app/views/cluster/repair-task-view/repair-task-view.component.ts
@@ -1,37 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DetailBaseComponent } from 'src/app/ViewModels/detail-table-base.component';
 import { ListColumnSetting } from 'src/app/Models/ListSettings';
 import { RepairTask } from 'src/app/Models/DataModels/repairTask';
 import { DataService } from 'src/app/services/data.service';
 import { forkJoin } from 'rxjs';
+import { RefreshService } from 'src/app/services/refresh.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-repair-task-view',
   templateUrl: './repair-task-view.component.html',
   styleUrls: ['./repair-task-view.component.scss']
 })
-export class RepairTaskViewComponent implements OnInit, DetailBaseComponent {
+export class RepairTaskViewComponent implements OnInit, DetailBaseComponent, OnDestroy {
   listSetting: ListColumnSetting;
   item: RepairTask;
   copyText = '';
   history: any;
   nodes = [];
-  constructor(public dataService: DataService) { }
+  constructor(public dataService: DataService, private refreshService: RefreshService) { }
 
   ngOnInit(): void {
     this.copyText = JSON.stringify(this.item.raw, null, '\t');
-  
-    console.log("test")
-    forkJoin(Array.from(new Set(this.item.raw.Target.NodeNames.concat(this.item.impactedNodes))).map(id => {
-      return this.dataService.getNode(id);
-    })).subscribe(data => {
-      this.nodes = data;
-      console.log(data);
-    })
+
+    this.updateNodesList().subscribe();
+
+    this.refreshService.insertRefreshSubject(this.item.id, () => this.updateNodesList());
+  }
+
+  ngOnDestroy() {
+    this.refreshService.removeRefreshSubject(this.item.id);
   }
 
   asIsOrder(a: any, b: any): number {
     return 1;
+  }
+
+  updateNodesList() {
+    return forkJoin(Array.from(new Set(this.item.raw.Target.NodeNames.concat(this.item.impactedNodes))).map(id => {
+      return this.dataService.getNode(id, true);
+    })).pipe(map(data => {
+      this.nodes = data;
+    }));
   }
 
 }
