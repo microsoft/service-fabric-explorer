6d32278daeed54b4c259e31c6fe30405b548e22e
jeffj6123 <jeffrey.jarry@gmail.com>
2021-05-19 20:03:33 -0700

    properly scroll the tree view when a item is selected outside of view window.

--- src/SfxWeb/src/app/modules/tree/tree-view/tree-view.component.ts
+++ src/SfxWeb/src/app/modules/tree/tree-view/tree-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, Output, EventEmitter, DoCheck, Input } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter, DoCheck, Input, AfterViewInit } from '@angular/core';
 import { TreeService } from 'src/app/services/tree.service';
 import { environment } from 'src/environments/environment';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
@@ -9,17 +9,22 @@ import { RestClientService } from 'src/app/services/rest-client.service';
   templateUrl: './tree-view.component.html',
   styleUrls: ['./tree-view.component.scss']
 })
-export class TreeViewComponent implements DoCheck {
+export class TreeViewComponent implements DoCheck, AfterViewInit {
 
   @Input() smallWindowSize = false;
   @Output() treeResize = new EventEmitter<number>();
 
   public showBeta = environment.showBeta;
   public canExpand = false;
+  @ViewChild('treeContainer') treeContainer: ElementRef;
   @ViewChild('tree') tree: ElementRef;
   constructor(public treeService: TreeService,
               private liveAnnouncer: LiveAnnouncer,
               public restClientService: RestClientService) { }
+
+  ngAfterViewInit() {
+    this.treeService.containerRef = this.treeContainer;
+  }
 
   ngDoCheck(): void {
     if (this.tree) {
