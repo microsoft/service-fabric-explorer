8b4db14664dae119657097dfbe8f2b5bed3a4de7
jeffj6123 <jeffrey.jarry@gmail.com>
2021-06-15 12:50:35 -0700

    emit information about whether or not a refresh was succesful.

--- src/SfxWeb/src/app/Models/DataModels/collections/CollectionBase.ts
+++ src/SfxWeb/src/app/Models/DataModels/collections/CollectionBase.ts
@@ -71,16 +71,20 @@ export class DataModelCollectionBase<T extends IDataModel<any>> implements IData
     }
 
     // Base refresh logic, do not override
-    public refresh(messageHandler?: IResponseMessageHandler): Observable<any> {
+    public refresh(messageHandler?: IResponseMessageHandler): Observable<boolean> {
+        let success = true;
         if (!this.refreshingPromise) {
             this.refreshingPromise = new AsyncSubject<any>();
 
             this.retrieveNewCollection(messageHandler).pipe(mergeMap(collection => {
                 return this.update(collection);
             }),
-            catchError( err => of(err))
+            catchError( err => {
+                success = false;
+                return of(err)
+            })
             ).subscribe( () => {
-                this.refreshingPromise.next(this);
+                this.refreshingPromise.next(success);
                 this.refreshingPromise.complete();
                 this.refreshingPromise = null;
             });
@@ -110,7 +114,7 @@ export class DataModelCollectionBase<T extends IDataModel<any>> implements IData
                 //     this.refreshingPromise = null;
                 // });
         }
-        return this.refreshingPromise ? this.refreshingPromise.asObservable() : of(null);
+        return this.refreshingPromise ? this.refreshingPromise.asObservable() : of(success);
     }
 
     public clear(): Observable<any> {
