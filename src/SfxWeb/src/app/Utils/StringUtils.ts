6eae66c49e148f8fab3d150538ec074e6e509a01
Daniel Gonzalez <daniel_gn98@hotmail.com>
2021-06-17 13:56:23 -0500

    Adding last refresh state (#567)
    * emit information about whether or not a refresh was successful. (#562)
    
    * emit information about whether or not a refresh was succesful.
    
    * linted.
    
    * added more checks for ensure initialized and removed any typing.
    
    * correctly pass in the instance from aad config (#565)
    
    * pass the instance
    
    * linted
    
    * retain the last refresh state (#566)
    
    * retain the last refresh state
    
    * linted
    
    Co-authored-by: jeffj6123 <jeffrey.jarry@gmail.com>

--- src/SfxWeb/src/app/Utils/StringUtils.ts
+++ src/SfxWeb/src/app/Utils/StringUtils.ts
@@ -1,7 +1,7 @@
-﻿//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License. See License file under the project root for license information.
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 export class StringUtils {
 
@@ -9,7 +9,7 @@ export class StringUtils {
      * Ensure the string ends with the specified suffix
      */
     public static EnsureEndsWith(str: string, suffix: string): string {
-        if (str.endsWith(suffix)) {
+        if (!str.endsWith(suffix)) {
             return str + suffix;
         }
         return str;
