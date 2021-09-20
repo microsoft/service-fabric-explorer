0044b22b44e76026ee9ab4aec0b3a6e9517ecc99
saketharsh <sharsh2010@gmail.com>
2021-09-20 11:55:26 +0530

    Most changes done and working

--- src/SfxWeb/src/app/Models/DataModels/completedInfrastructureJob.ts
+++ src/SfxWeb/src/app/Models/DataModels/completedInfrastructureJob.ts
@@ -7,11 +7,11 @@ import { StatusWarningLevel } from 'src/app/Common/Constants';
 import { IResponseMessageHandler } from 'src/app/Common/ResponseMessageHandlers';
 import { DataService } from 'src/app/services/data.service';
 
-export class InfrastructureJob extends DataModelBase<IRawInfrastructureJob> {
+export class CompletedInfrastructureJob extends DataModelBase<IRawInfrastructureJob> {
     
+   
     Id: string;
-    RepairTask:InfraRepairTask;
-    NodeImpact: string[];
+    ImpactAction: string;
 
    public get id(): string {
        return this.raw.Id;
@@ -23,9 +23,8 @@ export class InfrastructureJob extends DataModelBase<IRawInfrastructureJob> {
    }
 
    updateInternal() {
-       this.RepairTask = this.raw.RepairTasks.length != 0 ?  new InfraRepairTask(this.raw.RepairTasks[0].TaskId, this.raw.RepairTasks[0].State) : new InfraRepairTask("None", "None");
-       this.NodeImpact = [];
-       this.raw.CurrentlyImpactedRoleInstances.forEach( nodeimpact =>this.NodeImpact.push(nodeimpact.Name + ':' + nodeimpact.ImpactTypes.toString()) );
-       return of(null);
+       //this.RepairTask = this.raw.RepairTasks.length != 0 ?  new InfraRepairTask(this.raw.RepairTasks[0].TaskId, this.raw.RepairTasks[0].State) : new InfraRepairTask("None", "None");
+       this.ImpactAction = this.raw.ImpactAction; 
+       //return of(null);
    }
 }
--- src/SfxWeb/src/app/views/service/infrastructurejobs/infrastructurejobs.component.ts
+++ src/SfxWeb/src/app/views/service/infrastructurejobs/infrastructurejobs.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { ServiceBaseControllerDirective } from '../ServiceBase';
-import { ListSettings, ListColumnSetting } from 'src/app/Models/ListSettings';
+import { ListSettings, ListColumnSetting, ListColumnSettingWithFilter, ListColumnSettingWithShorten } from 'src/app/Models/ListSettings';
 import { DataService } from 'src/app/services/data.service';
 import { SettingsService } from 'src/app/services/settings.service';
-import { IResponseMessageHandler, ResponseMessageHandlers } from 'src/app/Common/ResponseMessageHandlers';
+import { IResponseMessageHandler } from 'src/app/Common/ResponseMessageHandlers';
 import { Observable } from 'rxjs';
-import { ActionCollection } from 'src/app/Models/ActionCollection';
-import { IsolatedAction, ActionWithConfirmationDialog } from 'src/app/Models/Action';
-import { mergeMap, map } from 'rxjs/operators';
-import { PartitionEnableBackUpComponent } from 'src/app/modules/backup-restore/partition-enable-back-up/partition-enable-back-up.component';
-import { PartitionDisableBackUpComponent } from 'src/app/modules/backup-restore/partition-disable-back-up/partition-disable-back-up.component';
+import { map } from 'rxjs/operators';
 import { TelemetryService } from 'src/app/services/telemetry.service';
 import { InfrastructureJob } from 'src/app/Models/DataModels/infrastructureJob';
 import { IRawInfrastructureJob } from 'src/app/Models/RawDataTypes';
-import { DashboardViewModel, IDashboardViewModel } from 'src/app/ViewModels/DashboardViewModels';
+import { CompletedInfrastructureJob } from 'src/app/Models/DataModels/completedInfrastructureJob';
+import { IEssentialListItem } from 'src/app/modules/charts/essential-health-tile/essential-health-tile.component';
+import { Constants } from 'src/app/Common/Constants';
 
 @Component({
   selector: 'app-infrastructurejobs',
@@ -21,41 +19,87 @@ import { DashboardViewModel, IDashboardViewModel } from 'src/app/ViewModels/Dash
   styleUrls: ['./infrastructurejobs.component.scss']
 })
 export class InfrastructureJobsComponent extends ServiceBaseControllerDirective {
-  allManagementJobs: InfrastructureJob[];
-  executingManagementJob : InfrastructureJob; 
-  listSettings : ListSettings;
-  tiles: IDashboardViewModel;
-
+  allPendingMRJobs: InfrastructureJob[];
+  executingMRJobs : InfrastructureJob[]; 
+  completedMRJobs : CompletedInfrastructureJob[];
+  allPendingMRJobsList: ListSettings;
+  executingMRJobsList: ListSettings;
+  completedMRJobsList : ListSettings;
+  executingInfraJobsSuggestion: string;
+  pendingInfraJobsSuggestion: string
 
   constructor(protected data: DataService, injector: Injector, private settings: SettingsService, public telemetry: TelemetryService) {
     super(data, injector);
   }
 
   setup() {
+    this.allPendingMRJobsList = this.settings.getNewOrExistingListSettings('allMRJobs', ['raw.CurrentUD'], [
+      new ListColumnSetting('raw.Id', 'Job Id'),
+      new ListColumnSettingWithFilter('raw.CurrentUD', 'Current UD'),
+      //new ListColumnSetting('raw.IsActive', 'Active'),
+      new ListColumnSetting('raw.AcknowledgementStatus', 'Acknowledgement Status'),
+      new ListColumnSetting('raw.ImpactAction', 'Impact Action'),
+     // new ListColumnSetting('raw.ImpactStep', 'Impact Step'),
+      new ListColumnSetting('RepairTask.TaskId', 'Repair Task'),
+      new ListColumnSettingWithShorten('raw.RoleInstancesToBeImpacted', 'Target Nodes',2),
+     ]);
+
+     this.executingMRJobsList = this.settings.getNewOrExistingListSettings('executingMRJobs', ['raw.IsActive'], [
+      new ListColumnSetting('raw.Id', 'Job Id'),
+      new ListColumnSettingWithFilter('raw.CurrentUD', 'Current UD'),
+      //new ListColumnSetting('raw.IsActive', 'Active'),
+      new ListColumnSetting('raw.AcknowledgementStatus', 'Acknowledgement Status'),
+      new ListColumnSetting('raw.ImpactAction', 'Impact Action'),
+      //new ListColumnSetting('raw.ImpactStep', 'Impact Step'),
+      new ListColumnSetting('RepairTask.TaskId', 'Repair Task'),
+      new ListColumnSetting('RepairTask.State', 'Repair Task State'),
+      new ListColumnSettingWithShorten('NodeImpact', 'Node Impact', 2)
+     ]);
+
+     this.completedMRJobsList = this.settings.getNewOrExistingListSettings('completedMRJobs', [], [
+      new ListColumnSetting('raw.Id', 'Job Id'),
+     // new ListColumnSettingWithFilter('raw.CurrentUD', 'Current UD'),
+      //new ListColumnSetting('raw.IsActive', 'Active'),
+     // new ListColumnSetting('raw.AcknowledgementStatus', 'Acknowledgement Status'),
+      new ListColumnSetting('raw.ImpactAction', 'Impact Action'),
+      //new ListColumnSetting('raw.ImpactStep', 'Impact Step'),
+     // new ListColumnSetting('RepairTask.TaskId', 'Repair Task'),
+      new ListColumnSetting('raw.RoleInstancesToBeImpacted', 'Nodes'),
+     ]);
+
     this.data.restClient.getInfrastructureJobs(this.serviceId).subscribe(mrJobdata =>  this.getInfrastructureData(mrJobdata));
   }
 
   getInfrastructureData(mrJobdata: IRawInfrastructureJob[]): void {
     const dateRef = new Date();
-     this.allManagementJobs = []
-      mrJobdata.forEach(rawMrJob => {
-        this.allManagementJobs.push(new InfrastructureJob(this.data, rawMrJob, dateRef));
-      })
 
-      this.listSettings = this.settings.getNewOrExistingListSettings('nodes', ['name'], [
-        new ListColumnSetting('raw.IsActive', 'Active'),
-        new ListColumnSetting('raw.ActionStatus', 'Action Status'),
-        new ListColumnSetting('raw.CurrentUD', 'Upgrade Domain'),
-        new ListColumnSetting('raw.ImpactAction', 'Impact Action'),
-        new ListColumnSetting('raw.ImpactStep', 'Impact Step'),
-        new ListColumnSetting('raw.Id', 'Job Id'),
-        new ListColumnSetting('RepairTask.TaskId', 'Repair Task')
-      ]);
-    this.executingManagementJob = this.allManagementJobs.find(job => Boolean(job.raw.IsActive) == true);
-    this.tiles = DashboardViewModel.fromHealthStateCount(this.executingManagementJob.id, this.executingManagementJob.raw.CurrentUD, false, {
-      ErrorCount: parseInt(this.executingManagementJob.raw.CurrentUD),
-      WarningCount: 0,
-      OkCount: 1,
-    })
+    this.executingMRJobs = [];
+    mrJobdata.filter(job => job.JobStatus == 'Executing' && Boolean(job.IsActive) == true).forEach(rawMrJob => {
+      this.executingMRJobs.push( new InfrastructureJob(this.data, rawMrJob, dateRef))
+    });
+
+    this.allPendingMRJobs = []
+    mrJobdata.filter(job => job.JobStatus != 'Completed'  && Boolean(job.IsActive) == false).forEach(rawMrJob => {
+      this.allPendingMRJobs.push(new InfrastructureJob(this.data, rawMrJob, dateRef));
+    });
+
+    this.completedMRJobs = []; 
+    mrJobdata.filter(job =>job.JobStatus == 'Completed').forEach(rawMrJob => {
+      this.completedMRJobs.push( new CompletedInfrastructureJob(this.data, rawMrJob, dateRef))
+    });
+    //this.infrastructureSuggestions = [];
+    //this.infrastructureSuggestions.push("Wow this is great");
+    if (this.executingMRJobs.length >=2 && this.allPendingMRJobs.length >0)
+    {
+     this.pendingInfraJobsSuggestion = "Jobs wont get approved because of Throttling policy in Infrastructure Service.  To know more about it, read here."
+    }
+    if (this.executingMRJobs.filter(job => job.raw.AcknowledgementStatus == 'WaitingForAcknowledgement').length >0)
+    {
+      this.executingInfraJobsSuggestion = "Check if the Corresponding Repair Task to the executing Infrastructure Job has any issues."
+    }
   };
+
+  refresh(messageHandler?: IResponseMessageHandler): Observable<any> {
+    return this.data.restClient.getInfrastructureJobs(this.serviceId, messageHandler).pipe(map(data => this.getInfrastructureData(data)));
+  }
 }
