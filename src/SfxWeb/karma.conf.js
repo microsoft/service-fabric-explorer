822ab76f78300593f7efcdb20f365944a0f29357
jeffj6123 <jeffrey.jarry@gmail.com>
2021-10-21 10:57:14 -0700

    pieces for generating voth parts (#636)

--- src/SfxWeb/karma.conf.js
+++ src/SfxWeb/karma.conf.js
@@ -12,12 +12,13 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/SfxWeb'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require('path').join(__dirname, './coverage-karma'),
+      reports: ['html', 'lcovonly', 'text-summary', 'json-summary', 'json'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
