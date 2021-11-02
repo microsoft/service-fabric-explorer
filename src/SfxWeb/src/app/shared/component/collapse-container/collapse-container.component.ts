8a5e4d926ce15b4df25bfdf0610398f2dba5d238
jeffj6123 <jeffrey.jarry@gmail.com>
2021-11-02 14:31:57 -0700

    handling last couple of bugs.

--- src/SfxWeb/src/app/shared/component/collapse-container/collapse-container.component.ts
+++ src/SfxWeb/src/app/shared/component/collapse-container/collapse-container.component.ts
@@ -1,12 +1,12 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collapse-container',
   templateUrl: './collapse-container.component.html',
   styleUrls: ['./collapse-container.component.scss']
 })
-export class CollapseContainerComponent implements OnChanges{
+export class CollapseContainerComponent implements OnChanges, OnInit{
 
   @Input() collapsed = false;
   @Input() disabled = false;
@@ -23,17 +23,23 @@ export class CollapseContainerComponent implements OnChanges{
     this.setText();
   }
 
+  ngOnInit() {
+    this.setText();
+  }
+
   changeCollapseState() {
+
     if (!this.disabled) {
-      this.liveAnnouncer.announce(this.collapsed ? 'Opened': 'Closed'  + this.sectionName + ' Section');
+      this.liveAnnouncer.announce(this.sectionName + ' Section has been ' + this.collapsed ? 'Opened': 'Closed' );
       this.collapsed = !this.collapsed;
       this.collapsedChange.emit(this.collapsed);
-      this.setText();
     }
+    this.setText();
+
   }
 
   setText() {
-    this.displayText = this.collapsed ? 'Open ': 'Close '  + this.sectionName + ' Section';
+    this.displayText = (this.collapsed ? 'Open ': 'Close ')  + this.sectionName + ' Section';
   }
 
 }
