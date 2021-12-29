0787249181ff7eb47e6fa797119ac501e78ee267
saketharsh <sharsh2010@gmail.com>
2021-12-29 10:31:26 +0530

    Final fixes

--- src/SfxWeb/src/app/Models/DataModels/infrastructureJob.ts
+++ src/SfxWeb/src/app/Models/DataModels/infrastructureJob.ts
@@ -25,14 +25,14 @@ export class InfrastructureJob extends DataModelBase<IRawInfrastructureJob> {
 
    updateInternal() {
        this.RepairTask = this.raw.RepairTasks.length !== 0 ?  new InfraRepairTask(this.raw.RepairTasks[0].TaskId, this.raw.RepairTasks[0].State) : new InfraRepairTask('None', 'None');
-       var repairTaskActual = this.data.repairCollection.collection.find(rt => rt.id === this.RepairTask.TaskId);
+       const repairTaskActual = this.data.repairCollection.collection.find(rt => rt.id === this.RepairTask.TaskId);
        this.VmImpact = [];
-       this.NodeImpact = []
-       if (repairTaskActual != null && repairTaskActual.raw.Impact != null)
+       this.NodeImpact = [];
+       if (repairTaskActual != null && repairTaskActual.raw.Impact !== null)
        {
            repairTaskActual.raw.Impact.NodeImpactList.forEach(nt => this.NodeImpact.push(nt.NodeName + ':' + nt.ImpactLevel) );
        }
-       if (this.RepairTask.State == 'Approved' || this.RepairTask.State == 'Executing' || this.RepairTask.State == 'Completed')
+       if (this.RepairTask.State === 'Approved' || this.RepairTask.State === 'Executing' || this.RepairTask.State === 'Completed')
        {
            this.raw.AcknowledgementStatus = 'Acknowledged';
        }
