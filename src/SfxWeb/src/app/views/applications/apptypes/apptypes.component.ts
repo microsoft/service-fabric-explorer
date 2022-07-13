6af3af77a8c6401e8860efd03ee5cf7e488d2cb6
Jeff <jeffrey.jarry@gmail.com>
2022-07-13 08:44:41 -0700

    application type for overview. (#689)
    * application type for overview.
    
    * added tests
    
    * fixed broken test.

---
+++ src/SfxWeb/src/app/views/applications/apptypes/apptypes.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apptypes',
+  templateUrl: './apptypes.component.html',
+  styleUrls: ['./apptypes.component.scss']
+})
+export class ApptypesComponent {
+
+  constructor() { }
+}
