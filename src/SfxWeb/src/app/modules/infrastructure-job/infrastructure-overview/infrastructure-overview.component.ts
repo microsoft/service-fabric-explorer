f1190f8838fc0d6ec666ccbdfbdf49b984b98a76
jeffj6123 <jeffrey.jarry@gmail.com>
2022-08-16 16:36:59 -0700

    removed old references to infra descriptions.

--- src/SfxWeb/src/app/modules/infrastructure-job/infrastructure-overview/infrastructure-overview.component.ts
+++ src/SfxWeb/src/app/modules/infrastructure-job/infrastructure-overview/infrastructure-overview.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { InfrastructureJob } from 'src/app/Models/DataModels/infrastructureJob';
-import { Constants } from 'src/app/Common/Constants';
-import { RepairTask } from 'src/app/Models/DataModels/repairTask';
 import { ListSettings, ListColumnSetting, ListColumnSettingWithFilter, ListColumnSettingWithShorten } from 'src/app/Models/ListSettings';
 import { SettingsService } from 'src/app/services/settings.service';
 import { RepairTaskCollection } from 'src/app/Models/DataModels/collections/RepairTaskCollection';
-import { DataService } from 'src/app/services/data.service';
 import { InfrastructureCollectionItem } from 'src/app/Models/DataModels/collections/infrastructureCollection';
 
 @Component({
@@ -13,7 +10,7 @@ import { InfrastructureCollectionItem } from 'src/app/Models/DataModels/collecti
   templateUrl: './infrastructure-overview.component.html',
   styleUrls: ['./infrastructure-overview.component.scss']
 })
-export class InfrastructureOverviewComponent implements OnInit, OnChanges {
+export class InfrastructureOverviewComponent implements OnInit {
   @Input() collection: InfrastructureCollectionItem;
   @Input() jobs: InfrastructureJob[];
   @Input() repairCollection: RepairTaskCollection;
@@ -21,8 +18,7 @@ export class InfrastructureOverviewComponent implements OnInit, OnChanges {
   allPendingMRJobsList: ListSettings;
   completedMRJobsList: ListSettings;
 
-  infrastructureJobsSuggestion: string[] = [];
-  constructor(private settings: SettingsService, private data: DataService) { }
+  constructor(private settings: SettingsService) { }
 
   ngOnInit(): void {
     this.allPendingMRJobsList = this.settings.getNewOrExistingInfrastructureSettings();
@@ -32,10 +28,5 @@ export class InfrastructureOverviewComponent implements OnInit, OnChanges {
       new ListColumnSetting('raw.ImpactAction', 'Impact Action'),
       new ListColumnSetting('raw.RoleInstancesToBeImpacted', 'Impacted Nodes'),
     ]);
-    this.infrastructureJobsSuggestion = [];
-  }
-
-  ngOnChanges(): void {
-    this.infrastructureJobsSuggestion = [];
   }
 }
