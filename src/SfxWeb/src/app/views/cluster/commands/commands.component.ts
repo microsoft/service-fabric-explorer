7c961fffc2d9f7bc269f3aea2908a9b06d93f498
chensation <fogwhite99@gmail.com>
2022-10-04 13:22:34 -0700

    add send health report to cluster

---
+++ src/SfxWeb/src/app/views/cluster/commands/commands.component.ts
@@ -0,0 +1,48 @@
+import { Component, Injector } from '@angular/core';
+import { CommandParamTypes, CommandSafetyLevel, PowershellCommand, PowershellCommandParameter } from 'src/app/Models/PowershellCommand';
+import { DataService } from 'src/app/services/data.service';
+import { BaseControllerDirective } from 'src/app/ViewModels/BaseController';
+
+@Component({
+  selector: 'app-commands',
+  templateUrl: './commands.component.html',
+  styleUrls: ['./commands.component.scss']
+})
+export class CommandsComponent extends BaseControllerDirective {
+  constructor(protected data: DataService, injector: Injector) {
+    super(injector);
+  }
+
+  commands: PowershellCommand[] = [];
+  
+  setup() {
+    this.setUpCommands();
+  }
+
+  
+  protected setUpCommands(): void {
+       
+    const healthState = new PowershellCommandParameter("HealthState", CommandParamTypes.enum, { options: ["OK", "Warning", "Error", "Unknown"], required: true});
+    const sourceId = new PowershellCommandParameter("SourceId", CommandParamTypes.string, {required: true});
+    const healthProperty = new PowershellCommandParameter("HealthProperty", CommandParamTypes.string, {required: true});
+    const description = new PowershellCommandParameter("Description", CommandParamTypes.string);
+    const ttl = new PowershellCommandParameter("TimeToLiveSec", CommandParamTypes.number);
+    const removeWhenExpired = new PowershellCommandParameter("RemoveWhenExpired", CommandParamTypes.bool)
+    const sequenceNum = new PowershellCommandParameter("SequenceNumber", CommandParamTypes.number);
+    const immediate = new PowershellCommandParameter("Immediate", CommandParamTypes.bool);
+    const timeoutSec = new PowershellCommandParameter("TimeoutSec", CommandParamTypes.number);
+    
+    const healthReport = new PowershellCommand(
+        'Send Health Report',
+        'https://docs.microsoft.com/powershell/module/servicefabric/send-servicefabricclusterhealthreport',
+        CommandSafetyLevel.unsafe,
+        'Send-ServiceFabricClusterHealthReport',
+        [healthState, sourceId, healthProperty, description, ttl, removeWhenExpired, sequenceNum, immediate, timeoutSec]
+    );
+
+    this.commands.push(healthReport);
+  }
+
+
+
+}
