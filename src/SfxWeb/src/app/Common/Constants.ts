5a84bfd7d3ef6838ef9876706f7effedec11af46
Jeff <jeffrey.jarry@gmail.com>
2023-02-17 15:47:37 -0500

    Timeline improvements (#829)
    * changed how colors are generated for the rca summary.
    
    * fixed the ES collapse button sizing.
    tooltips follow mouse movements
    fixed the UTC flipping and moved it out to a more obvious spot.
    Added more specific tooltip time label.
    
    * changed how colors are generated for the rca summary.
    
    * fixed the ES collapse button sizing.
    tooltips follow mouse movements
    fixed the UTC flipping and moved it out to a more obvious spot.
    Added more specific tooltip time label.
    
    * set utc from existing function.

--- src/SfxWeb/src/app/Common/Constants.ts
+++ src/SfxWeb/src/app/Common/Constants.ts
@@ -1,4 +1,4 @@
-// -----------------------------------------------------------------------------
+﻿// -----------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License. See License file under the project root for license information.
 // -----------------------------------------------------------------------------
