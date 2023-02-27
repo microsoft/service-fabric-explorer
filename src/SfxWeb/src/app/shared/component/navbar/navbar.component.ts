544fa53c29231cb843b34d5780c0e26557c6e5b0
chensation <fogwhite99@gmail.com>
2023-02-27 13:43:28 -0500

    fix typos

--- src/SfxWeb/src/app/shared/component/navbar/navbar.component.ts
+++ src/SfxWeb/src/app/shared/component/navbar/navbar.component.ts
@@ -16,7 +16,7 @@ export class NavbarComponent {
   @Input() actions: ActionCollection;
   @Input() showCopy = true;
   @Input() armManaged = false;
-  @Input() armWarningText = "This is an ARM managed resource. ARM amanged resources should only be modified during ARM deployments.";
+  @Input() armWarningText = "This is an ARM managed resource. ARM managed resources should only be modified during ARM deployments.";
 
   constructor(private router: Router) { }
 
