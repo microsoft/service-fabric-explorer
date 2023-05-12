e0480fb3703d93b018eec193e3c2682ed26f2e1b
chensation <fogwhite99@gmail.com>
2023-05-12 13:59:04 -0700

    reworked apptype-viewer to be reused by apptype and apps; apps now check for apptype as well

--- src/SfxWeb/src/app/views/applications/applicationsBase.ts
+++ src/SfxWeb/src/app/views/applications/applicationsBase.ts
@@ -1,23 +1,34 @@
 import { DataService } from 'src/app/services/data.service';
 import { Injector, Directive } from '@angular/core';
 import { IResponseMessageHandler } from 'src/app/Common/ResponseMessageHandlers';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, forkJoin } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 import { BaseControllerDirective } from 'src/app/ViewModels/BaseController';
-import { ApplicationCollection } from 'src/app/Models/DataModels/collections/Collections';
+import { ApplicationCollection, IAppTypeUsage } from 'src/app/Models/DataModels/collections/Collections';
 
 @Directive()
 export class ApplicationsBaseControllerDirective extends BaseControllerDirective {
 
     apps: ApplicationCollection;
+    usage: IAppTypeUsage;
+    hasArmManagedAppType: boolean;
 
     constructor(protected data: DataService, injector: Injector) {
       super(injector);
     }
 
     common(messageHandler?: IResponseMessageHandler): Observable<any> {
-        return this.data.getApps(true, messageHandler).pipe(map(apps => {
+        return forkJoin([
+          this.data.getApps(true, messageHandler).pipe(map(apps => {
             this.apps = apps;
-          }));
+          })),
+
+          this.data.appTypeGroups.ensureInitialized(true, messageHandler).pipe(mergeMap(() => {
+            return this.data.appTypeGroups.getAppTypeUsage().pipe(map(usage => {
+              this.usage = usage;
+              this.hasArmManagedAppType = this.usage.activeAppTypes.concat(this.usage.inactiveAppTypes).some(appType => appType.isArmManaged);
+            }))
+          }))
+        ]);
     }
   }
