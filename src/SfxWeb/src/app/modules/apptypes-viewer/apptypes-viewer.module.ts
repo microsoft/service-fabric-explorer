c199b0df9ebd287aae7a148e94d9e5cc8e7c1a06
chensation <fogwhite99@gmail.com>
2023-08-18 17:07:48 -0400

    fix 24742265: info icon not responding

--- src/SfxWeb/src/app/modules/apptypes-viewer/apptypes-viewer.module.ts
+++ src/SfxWeb/src/app/modules/apptypes-viewer/apptypes-viewer.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApptypeViewerComponent } from './apptype-viewer/apptype-viewer.component';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbNavModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { DetailListTemplatesModule } from '../detail-list-templates/detail-list-templates.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -14,6 +14,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     NgbNavModule,
+    NgbTooltipModule,
     DetailListTemplatesModule,
     SharedModule
   ],
