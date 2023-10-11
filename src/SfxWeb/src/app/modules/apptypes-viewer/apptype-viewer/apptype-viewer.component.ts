012d8a2ceb5d070e6e56a7cb626a7b262254c985
chensation <fogwhite99@gmail.com>
2023-10-11 16:22:00 -0400

    fixed coding practices, cleared up label message

--- src/SfxWeb/src/app/modules/apptypes-viewer/apptype-viewer/apptype-viewer.component.ts
+++ src/SfxWeb/src/app/modules/apptypes-viewer/apptype-viewer/apptype-viewer.component.ts
@@ -1,16 +1,15 @@
-import { Component, Injector, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ApplicationType } from 'src/app/Models/DataModels/ApplicationType';
 import { ListSettings } from 'src/app/Models/ListSettings';
 import { DataService } from 'src/app/services/data.service';
 import { SettingsService } from 'src/app/services/settings.service';
-import { BaseControllerDirective } from 'src/app/ViewModels/BaseController';
 
 @Component({
   selector: 'app-apptype-viewer',
   templateUrl: './apptype-viewer.component.html',
   styleUrls: ['./apptype-viewer.component.scss']
 })
-export class ApptypeViewerComponent extends BaseControllerDirective implements OnChanges {
+export class ApptypeViewerComponent implements OnChanges {
 
   @Input() activeAppTypes: ApplicationType[];
   @Input() inactiveAppTypes: ApplicationType[];
@@ -18,17 +17,11 @@ export class ApptypeViewerComponent extends BaseControllerDirective implements O
   allAppTypesListSettings: ListSettings;
   appTypesListSettings: ListSettings;
 
-  constructor(public dataService: DataService, private settings: SettingsService, injector: Injector) {
-    super(injector);
-  }
-
-  setup(): void {
-    this.allAppTypesListSettings = this.settings.getNewOrExistingAppTypeListSettings(true);
-    this.appTypesListSettings = this.settings.getNewOrExistingAppTypeListSettings();
-    this.allAppTypes = this.activeAppTypes.concat(this.inactiveAppTypes);
-  }
+  constructor(public dataService: DataService, private settings: SettingsService) {}
 
   ngOnChanges(): void { 
+    this.allAppTypesListSettings = this.settings.getNewOrExistingAppTypeListSettings(true);
+    this.appTypesListSettings = this.settings.getNewOrExistingAppTypeListSettings();
     this.allAppTypes = this.activeAppTypes.concat(this.inactiveAppTypes);
   }
 
