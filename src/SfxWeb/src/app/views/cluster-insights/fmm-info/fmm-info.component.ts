8ea1a0ee855719113fc242478a54d2415c3f359c
Crystal Wu <crystalwu@microsoft.com>
2026-01-25 08:46:10 -0800

    fmm info fallback

--- src/SfxWeb/src/app/views/cluster-insights/fmm-info/fmm-info.component.ts
+++ src/SfxWeb/src/app/views/cluster-insights/fmm-info/fmm-info.component.ts
@@ -10,6 +10,7 @@ import { RestClientService } from 'src/app/services/rest-client.service';
 export class FailoverManagerManagerInformationComponent implements OnInit {
   fmmInfo: IRawFailoverManagerManagerInformation = {} as IRawFailoverManagerManagerInformation;
   isLoading = true;
+  isFmmEstimate = false;
   
   constructor(private restClientService: RestClientService) {}
 
@@ -22,6 +23,40 @@ export class FailoverManagerManagerInformationComponent implements OnInit {
     this.restClientService.getFailoverManagerManagerInformation().subscribe({
       next: (data) => {
         this.fmmInfo = data;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.estimateFmmNode();
+      }
+    });
+  }
+
+  private estimateFmmNode(): void {
+    this.restClientService.getNodes().subscribe({
+      next: (nodes) => {
+        if (nodes && nodes.length > 0) {
+          // FMM is on the up node with the lowest node ID
+          const upNodes = nodes.filter(node => node.NodeStatus === "Up");
+          
+          if (upNodes.length > 0) {
+            let lowest = upNodes[0];
+            
+            upNodes.forEach(node => {
+              if (parseInt(lowest.Id.Id, 16) > parseInt(node.Id.Id, 16)) {
+                lowest = node;
+              }
+            });
+
+            if (lowest) {
+              this.fmmInfo = {
+                NodeName: lowest.Name,
+                NodeId: lowest.Id,
+                NodeInstanceId: lowest.InstanceId
+              };
+              this.isFmmEstimate = true;
+            }
+          }
+        }
         this.isLoading = false;
       },
       error: () => {
